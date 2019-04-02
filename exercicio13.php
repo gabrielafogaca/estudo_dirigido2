109c04a852499f97ca95baeda9df1f21e341c179
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-04-02 08:43:54 -0300

    Add files via upload

--- exercicio13.php
+++ exercicio13.php
@@ -1,15 +1,10 @@
-  
 <?php
+
 $num=101;
 $contagem=100;
-print "Começando em ...3";
-       sleep(1) ;
-print  " ...2";
-       sleep(1);
-print  " ...1";
-       sleep(1);
-print  " ...0 ";
-       sleep(1);
+
+print "Números de 100 a 1:";
+
 while ($contagem>=1)
     {$num--;   
      print "$num ";        
