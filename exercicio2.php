7e523e13c0c2a9efb503ef3a2d308951a2294c03
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-04-02 08:08:53 -0300

    Add files via upload

---
+++ exercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+    print "Digite um número: ";
+    $numero = fgets (STDIN);
+
+    if ($numero < 0){
+        print "Número negativo \n";
+    }
+    else{
+        print "Número positivo \n";
+    }
+    
