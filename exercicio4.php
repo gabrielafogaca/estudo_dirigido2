2367233114581595f883197770ba32ee41522500
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-04-09 10:51:50 -0300

    Update exercicio4.php

--- exercicio4.php
+++ exercicio4.php
@@ -1,3 +1,16 @@
 <?php 
 
+print "Dgite uma letra:";
+$letra =  (string) fgets (STDIN);​
+3
+    //4. Faça um algoritmo que verifique se uma letra digitada é vogal ou consoante. 
+
+if ($letra = "a" or "e" or "i" or "o" or "u") {
+print "A letra é uma vogal!";
+}
+
+else {
+print "A letra é uma consoante";  
+}​
+3
     //4. Faça um algoritmo que verifique se uma letra digitada é vogal ou consoante. 
