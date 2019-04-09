d89d6b1401b63fbfeed444d868858870b6fd8572
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-04-09 10:21:48 -0300

    Update exercicio8.php

--- exercicio8.php
+++ exercicio8.php
@@ -1,20 +1,20 @@
-<?php
+?php
 
-    print "Produto 1 - R$";
+    print "Digite o preço do produto 1 - R$";
     $produto1 = fgets (STDIN);
 
-    print "Produto 2 - R$";
+    print "Digite o preço do produto 2 - R$";
     $produto2 = fgets (STDIN);
 
-    print "Produto 3 - R$";
+    print "Digite o preço do produto 3 - R$";
     $produto3 = fgets (STDIN);
 
     if (($produto1<$produto2) and ($produto1<$produto3)){
-        print "O produto com menor preço é o Produto 1 | R$$produto1 \n";
+        print "O produto com menor preço é o Produto 1 | R$$produto1";
     }
     elseif (($produto2 < $produto1) and ($produto2<$produto3)){
-        print "O produto com menor preço é o Produto 2 | R$$produto2 \n";
+        print "O produto com menor preço é o Produto 2 | R$$produto2";
     }
     else{
-        print "O produto com menor preço é o Produto 3 | R$$produto3 \n";
-    
+        print "O produto com menor preço é o Produto 3 | R$$produto3";
+    }
