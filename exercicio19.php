896cdbc634c754d2a5fddf8d7c5b549b0b499e12
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-04-09 11:09:23 -0300

    Update exercicio19.php

--- exercicio19.php
+++ exercicio19.php
@@ -1,16 +1,19 @@
 <?php
-
 print "Série de Finabocci";
-print "A série vai até que o valor seja maior que 500."
-$fibonacci= (float) fgets(STDIN);
+print "A série vai até que o valor seja maior que 500.";
 
+
+sleep(1);
 $fibonacci=0;
 $fibonaccinovo=1;
 
 print "$fibonacci $fibonaccinovo ";
+while ($fibonaccinovo1<$fibonacci);
+    {$fibonaccinovo1=$fibonacci+$fibonaccinovo;
 
-while ($fibonaccinovo1<$fibonacci)
-    {$fibonaccinovoa1=$fibonacci+$fibonaccinovo;
      print "$fibonaccinovo1 ";
      $fibonacci= $fibonaccinovo;
      $fibonaccinovo= $fibonaccinovo1;}
+
+
+       
